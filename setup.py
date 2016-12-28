ad9d8797cd667b1f4bdf6dd57671bcae8d7fcbab
pedram26 <pedram_764@yahoo.com>
2016-12-27 20:57:53 -0800

    Add pip files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyMarkov',
+  packages = ['pyMarov'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Text Processing with Markov',
+  author = 'Pedram Pourdavood',
+  author_email = 'pedrampourdavood@gmail.com',
+  url = 'https://github.com/Pedram26/pyMarkov', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
