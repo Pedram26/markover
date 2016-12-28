00156c715703791e7951a87fda50b57f065b3c3d
pedram26 <pedram_764@yahoo.com>
2016-12-28 04:29:45 -0800

    Change default chunk

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
   name = 'markover',
   packages = ['markover'], # this must be the same as the name above
   scripts = ['markover/markover'],
-  version = '0.3',
-  description = 'Text Processing with Markov',
+  version = '0.4',
+  description = 'Natural Language Generator with Markov',
   author = 'Pedram Pourdavood',
   author_email = 'pedrampourdavood@gmail.com',
-  url = 'https://github.com/Pedram26/pyMarkov', # use the URL to the github repo
-  download_url = 'https://github.com/pedram26/pyMarkov/tarball/0.1',
-  keywords = ['file', 'text processing', 'markov'], # arbitrary keywords
+  url = 'https://github.com/Pedram26/markover', # use the URL to the github repo
+  download_url = 'https://github.com/pedram26/markover/tarball/0.4',
+  keywords = ['file', 'text processing', 'natural language' 'markov'], # arbitrary keywords
   classifiers = [],
 )
