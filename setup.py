96878b9f1cd3b36f6fb368ccf6ffc5860f851268
pedram26 <pedrampourdavood@gamil.com>
2017-04-23 01:09:40 -0700

    add long description to pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
+
+with open("README.rst, "rb") as f:
+    long_description = f.read()
+
 setup(
   name = 'markover',
   packages = ['markover'], # this must be the same as the name above
   scripts = ['markover/markover'],
   version = '0.4',
   description = 'Natural Language Generator with Markov',
+  long_description = long_description,
   author = 'Pedram Pourdavood',
   author_email = 'pedrampourdavood@gmail.com',
   url = 'https://github.com/Pedram26/markover', # use the URL to the github repo
