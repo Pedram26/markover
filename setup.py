f8346de40963b422382873d38899bda2e906fb4d
pedram26 <pedrampourdavood@gamil.com>
2017-04-23 02:22:39 -0700

    Update to new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   name = 'markover',
   packages = ['markover'], # this must be the same as the name above
   scripts = ['markover/markover'],
-  version = '0.5',
+  version = '0.6',
   description = 'Natural Language Generator with Markov',
   long_description = long_description,
   author = 'Pedram Pourdavood',
