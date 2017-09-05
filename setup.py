6856e94c5975d6dd6116a524a8af2e624e0a41f5
Pedram Pourdavood <pedrampourdavood@gmail.com>
2017-09-04 23:07:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from distutils.core import setup
 
+with open("README.rst", "rb") as f:
+    long_description = f.read()
+
 
 setup(
   name = 'markover',
   packages = ['markover'], # this must be the same as the name above
   scripts = ['markover/markover'],
-  version = '1.8',
+  version = '1.9',
   description = 'Natural Language Generator with Markov',
-  # long_description = long_description,
+  long_description = long_description,
   author = 'Pedram Pourdavood',
   author_email = 'pedrampourdavood@gmail.com',
   url = 'https://github.com/Pedram26/markover', # use the URL to the github repo
-  download_url = 'https://github.com/pedram26/markover/tarball/2.2',
+  download_url = 'https://github.com/pedram26/markover/tarball/2.3',
   keywords = ["file", "text processing", "natural language", "markov"], # arbitrary keywords
   classifiers = []
 )
