8697875708373800d92f22055971e1508417c1eb
Pedram Pourdavood <pedrampourdavood@gmail.com>
2017-09-04 22:22:31 -0700

    Added markover.egg-info

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from distutils.core import setup
 
-# with open("README.rst", "rb") as f:
-#     long_description = f.read()
+with open("README.rst", "rb") as f:
+    long_description = f.read()
 
 setup(
   name = 'markover',
   packages = ['markover'], # this must be the same as the name above
   scripts = ['markover/markover'],
-  version = '1.1',
+  version = '1.2',
   description = 'Natural Language Generator with Markov',
   # long_description = long_description,
   author = 'Pedram Pourdavood',
   author_email = 'pedrampourdavood@gmail.com',
   url = 'https://github.com/Pedram26/markover', # use the URL to the github repo
-  download_url = 'https://github.com/pedram26/markover/archive/1.5.tar.gz',
+  download_url = 'https://github.com/pedram26/markover/archive/1.6.tar.gz',
   keywords = ["file", "text processing", "natural language", "markov"], # arbitrary keywords
   classifiers = []
 )
