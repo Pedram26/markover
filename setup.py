1e8767fdc2b578936e3314597916a648e70ac70e
Pedram Pourdavood <pedrampourdavood@gmail.com>
2017-09-04 20:59:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
   author = 'Pedram Pourdavood',
   author_email = 'pedrampourdavood@gmail.com',
   url = 'https://github.com/Pedram26/markover', # use the URL to the github repo
-  download_url = 'https://github.com/pedram26/markover/archive/0.5.tar.gz',
+  download_url = 'https://github.com/pedram26/markover/archive/0.6.tar.gz',
   keywords = ['file', 'text processing', 'natural language' 'markov'], # arbitrary keywords
   classifiers = []
 )
