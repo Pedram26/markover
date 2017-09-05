ac86739639adf91e2f97f329fb50bc3236547a48
Pedram Pourdavood <pedrampourdavood@gmail.com>
2017-09-04 21:53:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
   name = 'markover',
   packages = ['markover'], # this must be the same as the name above
   scripts = ['markover/markover'],
-  version = '1.0',
+  version = '1.1',
   description = 'Natural Language Generator with Markov',
   # long_description = long_description,
   author = 'Pedram Pourdavood',
   author_email = 'pedrampourdavood@gmail.com',
   url = b'https://github.com/Pedram26/markover'.decode("utf-8"), # use the URL to the github repo
-  download_url = b'https://github.com/pedram26/markover/archive/1.4.tar.gz'.decode("utf-8"),
+  download_url = b'https://github.com/pedram26/markover/archive/1.5.tar.gz'.decode("utf-8"),
   keywords = ["file", "text processing", "natural language", "markov"], # arbitrary keywords
   classifiers = []
 )
